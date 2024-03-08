800c911b6c07bfc6bb984501d2b3c74f40372fcd
JuanFelixD2D <155562102+JuanFelixD2D@users.noreply.github.com>
2024-03-08 12:36:27 -0500

    Update store.php

--- view/home/store.php
+++ view/home/store.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("controller/homeController.php");
+    require_once("../../controller/homeController.php");
     $obj = new homeController();
     $correo = $_POST['correo'];
     $contraseña = $_POST['contraseña'];
